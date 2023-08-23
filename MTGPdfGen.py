16f2d99be8f4b2641d0f5017fd88866db7a1ce8a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-23 20:25:21 -0300

    all working, mtg bad res

--- MTGPdfGen.py
+++ MTGPdfGen.py
@@ -1,9 +1,16 @@
 import PyPDF2
+from PIL import Image, ImageFile
+import urllib.request
+from mtgsdk import Card
+import time
+
+ImageFile.LOAD_TRUNCATED_IMAGES=True
+starttime = time.time()
 
 def generatePdfFromCardArray():
     images = getCardsFromFile('./input/mtgInput.txt')
     images[0].save(
-    'output/mtgOutput.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
+    'output/mtgOutput.pdf', "PDF", save_all=True, append_images=images[1:]
 )
 
 def getCardsFromFile(pathToFile):
@@ -12,18 +19,35 @@ def getCardsFromFile(pathToFile):
         lines = f.readlines()
         f.close()
     for line in lines:
-        cardname = line.rstrip()
-        if(checkIfDualFaced(cardname)):
-            cardImages.append(getCardByName(getBackside(cardname)))
-        cardImages.append(getCardByName())
-            
+        fullCardname = line.rstrip()
+        amount = fullCardname.split(" ")[0]
+        for i in range (int(amount)):
+            cardInfo = getCardInfo(fullCardname)
+            cardnames = getCardNames(getCardByNameAndInfo(cardInfo[0], cardInfo[1], cardInfo[2]))
+            for name in cardnames:
+                time.sleep(1.0 - ((time.time() - starttime) % 1.0))
+                cardImages.append(getCardImageByName(name, cardInfo[1], cardInfo[2]))
+                print("Loaded card: " + fullCardname)
     return cardImages
 
-def getCardByName():
-    return
+def getCardInfo(fullCardname):
+    return [fullCardname.split("(")[0].split(" ", 1)[1].rsplit(" ", 1)[0], fullCardname.split("(")[1].split(")")[0], fullCardname.split(")")[1].split(" ")[1]]
 
-def checkIfDualFaced(cardname):
-    return
+def getCardByNameAndInfo(cardname, setCode, collectorsNumber):
+    card = Card.where(name=cardname).where(set=setCode).where(number=collectorsNumber).all()[0]
+    return card
 
-def getBackside(cardname):
-    return
+def getCardByName(cardname):
+    return Card.where(name=cardname)
+
+def getCardNames(card: Card): 
+    return card.names if card.names else [card.name]
+
+def getCardImageByName(cardname, setCode, collectorsNumber):
+    card: Card = getCardByNameAndInfo(cardname, setCode, collectorsNumber)
+    urllib.request.urlretrieve((card.image_url), 'C:\\Users\Felipe Bianco\Desktop\\temp')
+    img = Image.open('C:\\Users\Felipe Bianco\Desktop\\temp')
+    img.getdata()
+    return img
+
+generatePdfFromCardArray()
--- YGOPdfGen.py
+++ YGOPdfGen.py
@@ -3,15 +3,17 @@ from PIL import Image
 import requests
 from io import BytesIO
 import urllib.request
+import time
 
 baseUrl = "https://images.ygoprodeck.com/images/cards/"
 baseUrl2 = ".jpg"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
+
 def generatePdfFromCardArray():
     images = getCardsFromFile('./input/ygoInput.txt')
     images[0].save(
-    'output/ygoOutput.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=images[1:]
+    'output/ygoOutput.pdf', "PDF", save_all=True, append_images=images[1:]
 )
 
 def getCardsFromFile(pathToFile):
@@ -21,13 +23,14 @@ def getCardsFromFile(pathToFile):
         f.close()
     for line in lines:
         cardImages.append(getCardById(line.rstrip()))
+        print("Loaded card: " + line.rstrip())
     return cardImages
 
 
 def getCardById(id):
-    response = requests.get(baseUrl + id + baseUrl2, headers=headers)
     urllib.request.urlretrieve((baseUrl + id + baseUrl2), 'C:\\Users\Felipe Bianco\Desktop\\temp')
     img = Image.open('C:\\Users\Felipe Bianco\Desktop\\temp')
+    img.getdata() ## Genuinamente no tengo idea por que pero hacer eso hace que la resolucion sea buena, sin esto es terrible (sleep no funciona)
     return img
 
 generatePdfFromCardArray()
