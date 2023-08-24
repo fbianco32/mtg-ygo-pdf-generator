6eb5722d8027fb410f7b5480b79e57f136ed622c
BuildTools <unconfigured@null.spigotmc.org>
2023-08-24 15:16:09 -0300

    real size, QoL

--- Main.py
+++ Main.py
@@ -2,30 +2,29 @@ import Utils
 import MTGCardService
 import YGOCardService
 import YGOYdkService
-import sys
 
 def main():
     try:
         Utils.makeTempDir()
-        option = input('Choose game: [Y]GO/[M]TG\n')
-        if(option == 'Y'):
+        option = input('Choose game: [Y]GO/[M]TG\n').lower()
+        if(option == 'y'):
             input('Move YDK files to input folder, then press [ENTER] to continue')
             print('Preparing YDK')
             YGOYdkService.prepareYDKs()
-            input('YDK prepared. Move ./output/ygoInput.txt to ./input/ygoInput.txt then press [ENTER] to continue')
+            input('YDK prepared. Press [ENTER] to continue')
             print('Fetching card images...')
             images = YGOCardService.getCardsFromFile('./input/ygoInput.txt')
             print('Assembling PDF...')
-            images[0].save('output/ygoOutput.pdf', "PDF", save_all=True, append_images=images[1:])
+            images[0].save('output/ygoOutput.pdf', "PDF", save_all=True, append_images=images[1:], dpi=(187,187))
             print('Done! PDF can be found in ./output/ygoOutput.pdf')
-        elif(option == 'M'):
+        elif(option == 'm'):
             print('Move decklist file to input folder, rename it to "mtgInput.txt", then press [ENTER] to continue')
             print('(The decklist should be in XMAGE .dck format, with the extension changed to .txt)')
             input('(Its recommended to make a big list with all cards and export via moxfield > xmage.dck)')
             print('Fetching cards...')
             images = MTGCardService.getCardsFromFile('./input/mtgInput.txt')
             print('Assembling PDF...')
-            images[0].save('output/mtgOutput.pdf', "PDF", save_all=True, append_images=images[1:])
+            images[0].save('output/mtgOutput.pdf', "PDF", save_all=True, append_images=images[1:], dpi=(196,196))
             print('Done! PDF can be found in ./output/mtgOutput.pdf')
         else:
             print('Invalid Game')
--- YGOYdkService.py
+++ YGOYdkService.py
@@ -4,7 +4,7 @@ import os
 script_dir = os.path.dirname(__file__)
 
 def prepareYDKs():
-    with open(script_dir + '/output/ygoInput.txt', 'w') as outfile:
+    with open(script_dir + '/input/ygoInput.txt', 'w') as outfile:
         for fname in os.listdir(script_dir + "/ydkInput"):
             with open(script_dir + "/ydkInput/" + fname) as infile:
                 for line in infile:
