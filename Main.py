1f1338b954218f9d589009f05009c7bd0931d68d
Joaquin Leon <leonjoaquin77@gmail.com>
2023-08-28 01:55:50 -0300

    Thingy

--- Main.py
+++ Main.py
@@ -2,11 +2,12 @@ import Utils
 import MTGCardService
 import YGOCardService
 import YGOYdkService
+import CustomCardService
 
 def main():
     try:
         Utils.makeTempDir()
-        option = input('Choose game: [Y]GO/[M]TG\n').lower()
+        option = input('Choose game: [Y]GO/[M]TG/[C]ustom\n').lower()
         if(option == 'y'):
             print('Move YDK files to input folder, then press [ENTER] to continue')
             print('(The YDKs should be exported directly from YGOProDeck, saved as .ydk)')
@@ -29,10 +30,19 @@ def main():
             Utils.assemblePDF(images, width, height, margin, '#13160d', 63, 88, 'mtgOutput') # bgColor for mtg is always black, dpi for MTG is always 196
            # images[0].save('output/mtgOutput.pdf', "PDF", save_all=True, append_images=images[1:], dpi=(196,196))
             print('Done! PDF can be found in ./output/mtgOutput.pdf')
+        elif(option == 'c'):
+            input('Move card images to input folder, then press [ENTER] to continue')
+            print('Fetching cards...')
+            images = CustomCardService.getCardsFromFile('./input/custom/')
+            dpi = int(input('Enter images DPI: '))
+            print('Assembling PDF...')
+            images[0].save('output/customOutput.pdf', "PDF", save_all=True, append_images=images[1:], dpi=(dpi,dpi))
+            print('Done! PDF can be found in ./output/customOutput.pdf')
         else:
             print('Invalid Game')
     except BaseException as e:
-        print("An error has occured, info: " + str(e))
+        print("An error has occured, info: ")
+        raise(e)
     finally:
         Utils.cleanTempDir()
 
