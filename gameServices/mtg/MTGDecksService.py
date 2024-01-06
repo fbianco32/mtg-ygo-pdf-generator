825be1f3c87a95b3749b4514ed851d8b434050e7
nahuelvarisco <naguvaris@gmail.com>
2024-01-06 13:01:40 -0300

    read input from root

--- gameServices/mtg/MTGDecksService.py
+++ gameServices/mtg/MTGDecksService.py
@@ -12,8 +12,8 @@ def prepareMTGDecks() -> int:
     cardCount = 0
     cards = []
 
-    for fname in os.listdir(Utils.getPath('../../mtgInput')):
-        with open(Utils.getPath('../../mtgInput', fname)) as infile:
+    for fname in os.listdir(Utils.getPath('mtgInput')):
+        with open(Utils.getPath('mtgInput', fname)) as infile:
             for line in infile.readlines():
                 if(line.rstrip() not in IGNORED_LINES):
                     if line[-1] != '\n':
@@ -21,7 +21,7 @@ def prepareMTGDecks() -> int:
                     cards.append(line)
                     cardCount += 1
     
-    with open(Utils.getPath('../../input', 'mtgInput.txt'), 'w+') as outfile:
+    with open(Utils.getPath('input', 'mtgInput.txt'), 'w+') as outfile:
         outfile.writelines(cards)
             
     return cardCount
--- gameServices/ygo/YGOYdkService.py
+++ gameServices/ygo/YGOYdkService.py
@@ -3,9 +3,9 @@ import Utils
 
 def prepareYDKs() -> int:
     cardCount = 0
-    with open(Utils.getPath('../../input', 'ygoInput.txt'), 'w') as outfile:
-        for fname in os.listdir(Utils.getPath('../../ydkInput')):
-            with open(Utils.getPath('../../ydkInput', fname)) as infile:
+    with open(Utils.getPath('input', 'ygoInput.txt'), 'w') as outfile:
+        for fname in os.listdir(Utils.getPath('ydkInput')):
+            with open(Utils.getPath('ydkInput', fname)) as infile:
                 for line in infile:
                     if(line.strip('\n').isnumeric()):
                         outfile.write(line)
