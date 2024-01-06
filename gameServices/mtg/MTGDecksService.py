3bfe91e5fff13df34051a1991403f035f2a73fe7
Daniel <danielituswapp@gmail.com>
2024-01-06 15:50:41 -0300

    feat: take CSVs from cubecobra as input

--- gameServices/mtg/MTGDecksService.py
+++ gameServices/mtg/MTGDecksService.py
@@ -1,5 +1,5 @@
 import os
-import Utils
+import csv
 from pathlib import Path
 
 IGNORED_LINES = [
@@ -14,7 +14,7 @@ def prepareMTGDecks() -> int:
     cards = []
 
     for fname in (Path.cwd()/'mtgInput').iterdir():
-        cardCount, cards = prepare_txt_deck(cardCount, cards, fname)
+        cardCount, cards = prepare_csv_deck(cardCount, cards, fname) if fname.suffix=='.csv' else  prepare_txt_deck(cardCount, cards, fname)  
 
     with open(Path.cwd()/'input'/'mtgInput.txt', 'w+') as outfile:
         outfile.writelines(cards)
@@ -29,4 +29,20 @@ def prepare_txt_deck(cardCount, cards, fname):
                     line += '\n'
                 cards.append(line)
                 cardCount += 1
+    return cardCount, cards
+
+def prepare_csv_deck(cardCount, cards, fname):
+    with open(fname,'r') as infile:
+        rows = csv.reader(infile) 
+        col_names = list(next(rows))
+        idxs = {"name" : col_names.index("name"),
+                "set" : col_names.index("Set"),
+                "code" : col_names.index("Collector Number")}
+        for row in rows: #arranca de la linea 2 porque ya hicimos next()
+            name = row[idxs["name"]]
+            set = row[idxs["set"]]
+            code = row[idxs["code"]]
+            card = f"1 {name} ({set}) {code}"
+            cards.append(card+'\n')
+            cardCount+=1
     return cardCount, cards
