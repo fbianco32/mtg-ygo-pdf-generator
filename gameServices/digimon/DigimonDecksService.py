2e19685e8af3139e27df276fd37da21d66aee21c
Daniel <danielituswapp@gmail.com>
2024-01-06 15:58:36 -0300

    fix: pathlib for digimon

--- gameServices/digimon/DigimonDecksService.py
+++ gameServices/digimon/DigimonDecksService.py
@@ -1,11 +1,12 @@
 import os
 import Utils
+from pathlib import Path
 
 def prepareDigimon() -> int:
   cardCount = 0
-  with open(Utils.getPath('input', 'digimonInput.txt'), 'w') as outfile:
-    for fname in os.listdir(Utils.getPath('digimonInput')):
-      with open(Utils.getPath('digimonInput', fname)) as infile:
+  with open(Path.cwd()/'input'/'digimonInput.txt', 'w') as outfile:
+    for fname in (Path.cwd()/'digimonInput').iterdir():
+      with open(fname, 'r') as infile:
         for line in infile:
           textInLine = line.split()
           quantity = int(textInLine[0])
